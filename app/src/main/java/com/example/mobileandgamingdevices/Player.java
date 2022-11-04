3179ef87ace3200a0d2d0d06089f36d70d737773
Tom Scott <tom.scott45456@gmail.com>
2022-11-04 22:46:29 +0000

    Dynamic player sprites
    The player sprites now change depending on the direction that the car is facing

--- app/src/main/java/com/example/mobileandgamingdevices/Player.java
+++ app/src/main/java/com/example/mobileandgamingdevices/Player.java
@@ -5,9 +5,11 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.Log;
 
-import com.example.mobileandgamingdevices.graphics.Sprite;
+import com.example.mobileandgamingdevices.graphics.TextureManager;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class Player
 {
@@ -27,14 +29,36 @@ public class Player
     private Vector2 m_size;
     private Vector2 m_acceleration = new Vector2();
     private double m_accelerationRate = 0.05d;
-    private Sprite m_sprite;
 
-    public Player(Vector2 position, Sprite sprite)
+    private enum SpriteDirection
+    {
+        Left, Right, Up, Down
+    }
+
+    private class CarSpritePOJO
+    {
+        public CarSpritePOJO(Integer bonnetID, Integer chassisID)
+        {
+            m_bonnetID = bonnetID;
+            m_chassisID = chassisID;
+        }
+
+        public final Integer m_chassisID, m_bonnetID;
+    }
+
+    private Map<SpriteDirection, CarSpritePOJO> m_directionalSprites;
+
+    public Player(Vector2 position)
     {
         m_position = position;
         m_size = new Vector2(96d, 96d);
         m_velocity = new Vector2(0d, m_speed);
-        m_sprite = sprite;
+
+        m_directionalSprites = new HashMap<>();
+        m_directionalSprites.put(SpriteDirection.Up, new CarSpritePOJO(400, 427));
+        m_directionalSprites.put(SpriteDirection.Down, new CarSpritePOJO(426, 399));
+        m_directionalSprites.put(SpriteDirection.Left, new CarSpritePOJO(453, 454));
+        m_directionalSprites.put(SpriteDirection.Right, new CarSpritePOJO(481, 480));
     }
 
     public void update()
@@ -42,6 +66,13 @@ public class Player
         if (m_canMove)
         {
             m_rotation += m_targetRotation * m_turningRate;
+            if (m_rotation <= -360)
+            {
+                m_rotation += 360;
+            } else if (m_rotation >= 360)
+            {
+                m_rotation -= 360;
+            }
 
             m_velocity = new Vector2(0d, m_speed);
 
@@ -61,7 +92,7 @@ public class Player
                 }
             }
 
-            Log.d("Acceleration  ", String.format("%f   %f", m_acceleration.x.floatValue(), m_acceleration.y.floatValue()));
+            // Log.d("Acceleration  ", String.format("%f   %f", m_acceleration.x.floatValue(), m_acceleration.y.floatValue()));
 
             m_velocity = Vector2.rotate(m_velocity, m_rotation);
             m_position = m_position.add(m_velocity);
@@ -78,18 +109,58 @@ public class Player
             // Rotate the canvas
             canvas.save();
 
-            final Vector2 topLeft = display.worldToScreenSpace(m_position);
+            SpriteDirection direction;
+            float spriteRotation = 0f;
 
-            final Vector2 bottomRight = display.worldToScreenSpace(new Vector2(
-                    m_position.x + m_size.x,
-                    m_position.y + m_size.y
+            /*
+             *             UP
+             *        315  360
+             *       -45   0    45
+             *          \  |  /
+             *           \ |/
+             *LEFT -90____ | ____90 RIGHT
+             *     270    /|\
+             *          /  | \
+             *     -135   180 \
+             *     225        135
+             *           DOWN
+             */
+
+            // Depending on the rotation, assign a different sprite
+            if ((m_rotation <= 45 && m_rotation >= -45) ||
+                    m_rotation >= 315)
+            {
+                direction = SpriteDirection.Up;
+                spriteRotation = 0f;
+            } else if ((m_rotation <= -45 && m_rotation >= -135) ||
+                    (m_rotation <= 315 && m_rotation >= 225))
+            {
+                direction = SpriteDirection.Left;
+                spriteRotation = 90f;
+            } else if ((m_rotation >= 45 && m_rotation <= 135) ||
+                    m_rotation <= -270 && m_rotation >= -360)
+            {
+                direction = SpriteDirection.Right;
+                spriteRotation = -90f;
+            } else
+            {
+                direction = SpriteDirection.Down;
+                spriteRotation = 180f;
+            }
+
+
+            Vector2 chassisTopLeft = display.worldToScreenSpace(m_position);
+
+            Vector2 bonnetTopLeft = display.worldToScreenSpace(new Vector2(
+                    m_position.x,
+                    m_position.y - m_size.y
             ));
 
+            //TODO: CENTRE OF CAR WILL CHANGE DEPENDING ON WHICH SPRITE ITS USING
             final Vector2 centre = display.worldToScreenSpace(new Vector2(
                     m_position.x + (m_size.x / 2d),
                     m_position.y + (m_size.y / 2d)
             ));
-
 
             canvas.rotate(
                     (float) m_rotation,
@@ -97,7 +168,13 @@ public class Player
                     centre.y.floatValue()
             );
 
-            m_sprite.draw(canvas, topLeft, m_size);
+
+            Log.d("ROTATION", String.valueOf(m_rotation) + "  FACING:  " + direction.toString());
+
+            CarSpritePOJO sprites = m_directionalSprites.get(direction);
+
+            TextureManager.getInstance().drawSprite(sprites.m_bonnetID, canvas, bonnetTopLeft, m_size, spriteRotation);
+            TextureManager.getInstance().drawSprite(sprites.m_chassisID, canvas, chassisTopLeft, m_size, spriteRotation);
 
 //            canvas.drawRect(
 //                    topLeft.x.floatValue(),
